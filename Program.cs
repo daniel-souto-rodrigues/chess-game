8ab34c5f57179503ffe36a8b8df953f7dd22536a
Daniel Rodrigues <dantdmc210@gmail.com>
2020-07-19 09:31:49 -0300

    handling errors with custom exceptions

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using board;
+﻿using System;
+using board;
 using chess;
 
 namespace chess_game
@@ -7,13 +8,23 @@ namespace chess_game
     {
         static void Main(string[] args)
         {
-            Board board = new Board(8, 8);
+            try
+            {
+                Board board = new Board(8, 8);
 
-            board.addPiece(new Tower(board, Color.black), new Position(0, 0));
-            board.addPiece(new Tower(board, Color.black), new Position(1, 3));
-            board.addPiece(new King(board, Color.black), new Position(2, 4));
+                board.addPiece(new Tower(board, Color.black), new Position(0, 0));
+                board.addPiece(new Tower(board, Color.black), new Position(1, 3));
+                board.addPiece(new King(board, Color.black), new Position(2, 4));
 
-            Screen.BoardPrint(board);
+                //test
+                board.addPiece(new King(board, Color.black), new Position(0, 2));
+
+                Screen.BoardPrint(board);
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- board/Board.cs
+++ board/Board.cs
@@ -20,10 +20,36 @@ namespace board
             return pieces[line, column];
         }
 
+        public Piece Piece(Position pos)
+        {
+            return pieces[pos.Line, pos.Column];
+        }
+
+        public bool PieceExist(Position pos)
+        {
+            PositionValidate(pos);
+            return Piece(pos) != null;
+        }
+
         public void addPiece(Piece p, Position pos)
         {
+            if (PieceExist(pos))
+                throw new BoardException("Already exist a piece on this position!");
             pieces[pos.Line, pos.Column] = p;
             p.Position = pos;
+        }
+
+        public bool validPositionTest(Position pos)
+        {
+            if (pos.Line < 0 || pos.Line >= Lines || pos.Column < 0 || pos.Column >= Columns)
+                return false;
+            return true;    
+        }
+
+        public void PositionValidate(Position pos)
+        {
+            if (!validPositionTest(pos))
+                throw new BoardException("Invalid Position!");
         }
     }
 }
